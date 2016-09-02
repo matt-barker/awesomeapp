15f004ec87a3b94fad1fabde60eebfd157e65ea0
Matt Barker <matt@mattbarker.net>
2016-09-02 13:55:25 +0000

    5.7 Create comment model with associations. Style products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
     has_many :orders
+    has_many :comments
+    
+    def highest_rated_comment
+        comments.rating_desc.first
+    end
+    
+    def lowest_rated_comment
+        comments.rating_asc.first
+    end
 end
