5b73ba59d4e24ca0674abb0bc3100829b7252b78
Matt Barker <matt@mattbarker.net>
2016-09-10 13:03:59 +0000

    5.10 Add admin role and prevent non admins from deleting comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -10,4 +10,5 @@ $(document).on('turbolinks:load', function(){
     
     //Fade the rails alerts after 5 seconds
     $('.alert').delay(5000).fadeOut(500);
+    
 });
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :destroy, Comment
+    else
+      can :manage, User, id: user.id
+    end
   end
 end
