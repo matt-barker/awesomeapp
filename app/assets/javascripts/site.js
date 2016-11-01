a0577a7f3a90cba96881df03646a14da9e9a2606
Matt Barker <matt@mattbarker.net>
2016-11-01 13:47:55 +0000

    6.10 Add AJAX for comment submission

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
-$(document).on('turbolinks:load', function(){
-    //Product ratings
+
+var refreshRatings = function(){
     $('.rating').raty({ path: '/images', scoreName: 'comment[rating]', });
     $('.rated').raty({ path: '/images',
       readOnly: true,
@@ -7,7 +7,11 @@ $(document).on('turbolinks:load', function(){
         return $(this).attr('data-score');
       }
     });
-    
+};
+
+$(document).on('turbolinks:load', function(){
+    //Product ratings
+    refreshRatings();
     //Fade the rails alerts after 5 seconds
     $('.alert').delay(5000).fadeOut(500);
     
